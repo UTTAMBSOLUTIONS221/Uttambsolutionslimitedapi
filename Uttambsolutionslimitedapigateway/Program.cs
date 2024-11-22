e40815e8d3718579b0383ffadb0cb28eab48d40d
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-11-22 10:51:35 +0300

    update

--- Uttambsolutionslimitedapigateway/Program.cs
+++ Uttambsolutionslimitedapigateway/Program.cs
@@ -1,6 +1,34 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// Add services to the container.
+//builder.Services.AddSwaggerGen();
 
-app.MapGet("/", () => "Hello World!");
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Replace with your frontend's origin
+              .AllowAnyHeader()                    // Allow all headers
+              .AllowAnyMethod()                   // Allow all HTTP methods
+              .AllowCredentials();               // Optional                 
+    });
+});
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("uttambsolutionslimitedocelot.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
+var app = builder.Build();
+//app.UseSwagger();
+//app.UseSwaggerUI();
+
+// Use Ocelot
+app.UseRouting();
+
+// Enable the CORS policy
+app.UseCors("AllowSpecificOrigin");
+app.UseOcelot().Wait();
 
 app.Run();
